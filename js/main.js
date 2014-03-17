cb1b6abfe4badd63e0ff331710ac438144b5c0ef
Brett Renfer <brett@robotconscience.com>
2014-03-17 15:29:08 -0400

    Switched to combined web app

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 var app;
 
 $(document).ready( function() {
-	App.prototype = $.extend(true, SUD.app.BaseApp.prototype, App.prototype);
-	app 	= new App();
+	DemoApp.prototype = $.extend(true, SUD.app.BaseApp.prototype, DemoApp.prototype);
+	app 	= new DemoApp();
 	app.begin();
 });
 
+// UTIL!
 function HSVtoRGB(h, s, v) {
     var r, g, b, i, f, p, q, t;
     if (h && s === undefined && v === undefined) {
@@ -30,162 +31,321 @@ function HSVtoRGB(h, s, v) {
         b: Math.floor(b * 255)
     };
 }
-	      
-// Spacebrew Object
-var sb, 
-	app_name = "grid_"
-	;
 
-var App = function(){
-	SUD.app.BaseApp.call( this );
+// ===========================================
+// ===== DEMO APP
+// ===========================================
 
-	this.r = 0;
-	this.g = 0;
-	this.b = 0;
+	DemoApp = function(){
+		SUD.app.BaseApp.call( this );
 
-	this.rE = 0;
-	this.gE = 0;
-	this.bE = 0;
+		var sb;
 
-	//-------------------------------------------------------
-	this.setup = function() {
-		var random_id = "0000" + Math.floor(Math.random() * 10000);
+		// color vars		
+		this.r = 0;
+		this.g = 0;
+		this.b = 0;
 
-		app_name = app_name + ' ' + random_id.substring(random_id.length-4);
-
-		// create spacebrew client object
-		sb = new Spacebrew.Client();
-		sb.send_interval = 0;
-
-		// set the base description
-		sb.name(app_name);
-		sb.description("This spacebrew example sends and receives custom messages.");
-
-		// configure the publication and subscription feeds
-		sb.addPublish( "red", "range", "0" );
-		sb.addPublish( "green", "range", "0" );
-		sb.addPublish( "blue", "range", "0" );
+		this.rE = 0;
+		this.gE = 0;
+		this.bE = 0;
 		
-		sb.addSubscribe( "mode", "range" );
-		sb.addSubscribe( "randomR", "range" );
-		sb.addSubscribe( "randomG", "range" );
-		sb.addSubscribe( "randomB", "range" );
+		//-------------------------------------------------------
+		this.setup = function (){
+			//this.unregisterMouseEvents();
+			this.registerTouchEvents();
 
-		// override Spacebrew events - this is how you catch events coming from Spacebrew
-		sb.onCustomMessage = onCustomMessage;
-		sb.onRangeMessage  = this.onRangeMessage.bind(this);
-		sb.onOpen = onOpen;
+			this.setupSpacebrew();
 
-		// connect to spacbrew
-		sb.connect();
+			// grid
+			this.grid = [];
+			for ( var x=0; x<10; x++){
+				for ( var y=0; y<10; y++){
+					this.grid.push({x: x, y: y, filled: false });
+				}
+			}
+			this.currentDrawing = [];
+			this.touchId 		= -1;
+			this.gridIndices = [];
 
-		document.getElementById("can").addEventListener("touchstart", this.onTouch.bind(this), false);
-		document.getElementById("can").addEventListener("touchmove", this.onTouch.bind(this), false);
-		document.getElementById("can").addEventListener("touchend", this.onTouchUp.bind(this), false);
-	}
+			this.gridCanvas = document.getElementById("gridCanvas");
+			// todo: set w/h
+			this.gridCtx 	= this.gridCanvas.getContext('2d');
 
-	//-------------------------------------------------------
-	this.update = function(){
-	}
+			// text
+			this.button = document.getElementById("button");
+			this.button.onclick = this.textPressed.bind(this);
 
-	//-------------------------------------------------------
-	this.draw 	= function(){
-		this.rE = this.rE * .75 + this.r * .25;
-		this.gE = this.gE * .75 + this.g * .25;
-		this.bE = this.bE * .75 + this.b * .25;
-		$("body").css("background-color", "rgb(" + Math.round(this.rE) +","+ Math.round(this.gE) +"," + Math.round(this.bE) + ")");
-	}
+			this.color = {};
+			this.color.r = SUD.randomInt(0,255);
+			this.color.g = SUD.randomInt(0,255);
+			this.color.b = SUD.randomInt(0,255);
 
-	//-------------------------------------------------------
-	this.onMouseMoved = function( x,y ){
-		
-	};
+			this.modes = [];
+			this.modes.push("#grid");
+			this.modes.push("#text");
+			this.modes.push("#color");
 
-	this.onTouch = function( e ){
-	    e.preventDefault();
-		this.onMousePressed(e.layerX, e.layerY);
-	}
-
-	this.onTouchUp = function( e ){
-	    e.preventDefault();
-
-		sb.send("red", "range", this.r);
-		sb.send("green", "range", this.g);
-		sb.send("blue", "range", this.b);
-	};
-
-	//-------------------------------------------------------
-	this.onMousePressed = function( x,y ){
-		var hue = SUD.clamp(SUD.map(x,0,window.innerWidth, 0, 1.0), 0,1.0);
-		var sat = SUD.clamp(SUD.map(y,0,window.innerHeight, 0, 1.0),0.,1.0);
-
-		var rgb = HSVtoRGB(hue, sat, sat)
-
-		this.r = Math.round(rgb.r);
-		this.g = Math.round(rgb.g);
-		this.b = Math.round(rgb.b);
-
-		sb.send("red", "range", this.r);
-		sb.send("green", "range", this.g);
-		sb.send("blue", "range", this.b);
-	};
-
-	//-------------------------------------------------------
-	this.onMouseDragged = function( x,y ){
-		this.onMousePressed(x,y);
-	};
-
-	//-------------------------------------------------------
-	this.onMouseReleased = function( x,y ){
-		
-	};
-
-	this.onRangeMessage = function( name, value ){
-		if ( name == "mode" ){
-
-		} else if ( name == "randomR" ){
-			this.r = Math.round(value);
-		} else if ( name == "randomG" ){
-			this.g = Math.round(value);
-		} else if ( name == "randomB" ){
-			this.b = Math.round(value);
+			window.scrollTo(0,1);
+			this.mode = 1;
 		}
-	}
-}
 
-/**
- * Function that is called when Spacebrew connection is established
- */
-function onOpen() {
-	var message = "Connected as <strong>" + sb.name() + "</strong>. ";
-	if (sb.name() === app_name) {
-		message += "<br>You can customize this app's name in the query string by adding <strong>name=your_app_name</strong>."
-	}
-	$("#name").html( message );
-}
+		//-------------------------------------------------------
+		this.setupSpacebrew = function() {
+			var random_id = "0000" + Math.floor(Math.random() * 10000);
+			var app_name = "Swipe_" + random_id.substring(random_id.length-4);
 
-/**
- * @param  {[type]} name  [description]
- * @param  {[type]} value [description]
- * @return {[type]}       [description]
- */
+			// create spacebrew client object
+			sb = new Spacebrew.Client();
+			sb.send_interval = 0;
+			sb.name(app_name);
 
+			// configure the publication and subscription feeds
+			// grid
+			sb.addPublish( "grid", "grid", "[]" );
 
-/**
- * onCustomMessage Function that is called whenever new spacebrew custom messages are received.
- *          It accepts three parameters:
- * @param  {String} name    Holds name of the subscription feed channel
- * @param  {String} value 	Holds value received from the subscription feed
- * @param  {String} type 	Holds the custom message type
- */
-function onCustomMessage( name, value, type ){
-	console.log("[onCustomMessage] remote dice roll ", value);
-	if (type = "dice") {
-		if (isNaN(value)) {
-			value = "you can't cheat the house, that's not a dice roll";
-		} else {
-			if (value > 6 || value <= 0) value = "you can't cheat the house, that's not a dice roll";
+			// color
+			sb.addPublish( "red", "range", "0" );
+			sb.addPublish( "green", "range", "0" );
+			sb.addPublish( "blue", "range", "0" );
+
+			// text
+			sb.addPublish( "text", "string" );
+			
+			// stuff we're subscribing to
+			sb.addSubscribe( "mode", "range" );
+			sb.addSubscribe( "randomR", "range" );
+			sb.addSubscribe( "randomG", "range" );
+			sb.addSubscribe( "randomB", "range" );
+
+			// override Spacebrew events - this is how you catch events coming from Spacebrew
+			sb.onRangeMessage  = this.onRangeMessage.bind(this);
+
+			sb.connect();
+		};
+
+		//-------------------------------------------------------
+		this.update = function (){
+			// color update
+			this.rE = this.rE * .75 + this.r * .25;
+			this.gE = this.gE * .75 + this.g * .25;
+			this.bE = this.bE * .75 + this.b * .25;
 		}
-		$("#msg_received").text(value); // display the sent message in the browser         
+
+		//-------------------------------------------------------
+		this.draw = function (){
+			switch( this.mode ){
+				// color
+				case 0:
+					$("#color").css("background-color", "rgb(" + Math.round(this.rE) +","+ Math.round(this.gE) +"," + Math.round(this.bE) + ")");
+					break;
+
+				// grid
+				case 1:
+					//console.log("grid")
+					var w = 50;
+					var x = parseInt(this.gridCanvas.width) / 2.0 - ((w * 9)/2.0);
+					var y = parseInt(this.gridCanvas.height) / 2.0 - ((w * 9) /2.0);
+					
+					this.gridCanvas.width = this.gridCanvas.width;
+					this.gridCtx.clearRect(0,0,this.gridCanvas.width,this.gridCanvas.height);
+			
+
+					this.gridCtx.fillStyle = ("rgb("+ this.color.r+"," + this.color.g + "," + this.color.b + ")");
+					this.gridCtx.strokeStyle = ("rgb("+ this.color.r+"," + this.color.g + "," + this.color.b + ")");
+
+					for ( var i=0; i<this.grid.length; i++){
+						this.gridCtx.beginPath();
+						this.gridCtx.arc( 	x + this.grid[i].x * w,
+										y + this.grid[i].y * w, 
+										(w * .8)/2,
+										0,2*Math.PI);
+						if ( this.grid[i].filled ){
+							this.gridCtx.fill();
+						} else {
+							this.gridCtx.stroke();
+						}
+					}
+					break;
+
+				// text
+				case 2:
+					break;
+			}
+		}	
+
+		//-------------------------------------------------------
+		this.updateAndSendColor = function(x,y){
+			var hue = SUD.clamp(SUD.map(x,0,window.innerWidth, 0, 1.0), 0,1.0);
+			var sat = SUD.clamp(SUD.map(y,0,window.innerHeight, 0, 1.0),0.,1.0);
+
+			var rgb = HSVtoRGB(hue, sat, sat)
+
+			this.r = Math.round(rgb.r);
+			this.g = Math.round(rgb.g);
+			this.b = Math.round(rgb.b);
+
+			sb.send("red", "range", this.r);
+			sb.send("green", "range", this.g);
+			sb.send("blue", "range", this.b);
+		}
+
+		//-------------------------------------------------------
+		this.checkGrid = function(x,y){
+
+			var w = 50;
+			var cx = parseInt(this.gridCanvas.width) / 2.0 - ((w * 10)/2.0);
+			var cy = parseInt(this.gridCanvas.height) / 2.0 - ((w * 10) /2.0);
+
+			for ( var i=0; i<this.grid.length; i++){
+				var rx = cx + this.grid[i].x * w;
+				var ry = cy + this.grid[i].y * w;
+
+				if ( x <= rx + w/2 && x >= rx - w/2 &&
+					 y <= ry + w/2 && y >= ry - w/2 ){
+					this.grid[i].filled = true;
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		//-------------------------------------------------------
+		this.textPressed = function(e){
+			var t = document.forms[0].textField.value;
+			console.log(t);
+			if ( t.length > 0 ){
+				sb.send("text", "string", t);
+				document.forms[0].textField.value = "";
+			}
+		}
+
+		//-------------------------------------------------------
+		this.onMousePressed = function( x,y ){
+			this.onTouchStart(1,x,y);
+		};
+
+		//-------------------------------------------------------
+		this.onMouseMoved = function( x,y ){
+			this.onTouchMove(1,x,y);
+		};
+
+		//-------------------------------------------------------
+		this.onMouseDragged = function( x,y ){
+			this.onTouchMove(1,x,y);
+		};
+
+		//-------------------------------------------------------
+		this.onMouseReleased = function( x,y ){
+			this.onTouchEnd(1,x,y);
+		};
+
+		//-------------------------------------------------------
+		this.onTouchStart = function( id, x,y ){
+			switch ( this.mode ){
+				case 0:
+					this.updateAndSendColor(x,y);
+					break;
+				case 1:
+					if ( this.touchId == -1 ){
+						this.touchId = id;
+						//this.currentDrawing.push({x:x, y:y});
+						
+						// are we over any grid squares?
+						var ind = this.checkGrid(x,y);
+						if ( ind != -1 ){
+							this.gridIndices.push(ind);
+						}
+					}
+					break;
+				case 2:
+					break;
+			}
+		};
+
+		//-------------------------------------------------------
+		this.onTouchMove = function( id, x,y ){
+			switch ( this.mode ){
+				case 0:
+					this.updateAndSendColor(x,y);
+					break;
+				case 1:
+					if ( id == this.touchId ){
+						//this.currentDrawing.push({x:x, y:y});
+						var ind = this.checkGrid(x,y);
+						if ( ind != -1 ){
+							this.gridIndices.push(ind);
+						}
+					}
+					break;
+				case 2:
+					break;
+			}
+		};
+
+		//-------------------------------------------------------
+		this.onTouchEnd = function( id, x,y ){
+			switch ( this.mode ){
+				case 0:
+					// this.updateAndSendColor(x,y);
+					break;
+				case 1:
+					if ( id == this.touchId ){
+						this.touchId = -1;
+
+						// send grid obj + color
+						sb.send("grid", "grid", {grid: this.grid, color: this.color, indices:this.gridIndices })
+
+						for ( var i=0; i<this.grid.length; i++){
+							this.grid[i].filled = false;
+						}
+
+						this.gridIndices = [];
+					}
+					break;
+				case 2:
+					break;
+			}
+		};
+
+		//-------------------------------------------------------
+		this.onRangeMessage = function( name, value ){
+			if ( name == "mode" ){
+				this.mode = value;
+				switch( value ){
+					// color
+					case 0:
+						this.showHide("#color");
+						break;
+
+					// grid
+					case 1:
+						this.showHide("#grid");
+						break;
+
+					// text
+					case 2:
+						this.showHide("#text");
+						break;
+				}
+			} else if ( name == "randomR" ){
+				this.r = Math.round(value);
+			} else if ( name == "randomG" ){
+				this.g = Math.round(value);
+			} else if ( name == "randomB" ){
+				this.b = Math.round(value);
+			}
+		};
+
+		this.showHide = function( show ) {
+			for ( var i=0; i<this.modes.length; i++){
+				$(this.modes[i]).css("visibility", "hidden");
+				$(this.modes[i]).css("display", "none");
+			}
+
+			$(show).css("visibility", "visible");
+			$(show).css("display", "block");
+		}
+
 	}
-}
+	
